dabb2e8975dbc83739890b54dc46a8c84c5f2f47
jean-francois Defez <jean-francois@ayctor.com>
2024-08-30 14:29:01 +0200

    refactor(tests)

--- tests/Unit/IsDtoTest.php
+++ tests/Unit/IsDtoTest.php
@@ -9,7 +9,24 @@ use Ayctor\Tests\Samples\UserStatus;
 use Carbon\Carbon;
 use Illuminate\Support\Collection;
 
-it('toArray returns an array', function () {
+it('hides properties', function () {
+    $dto = UserDto::make([
+        'firstname' => 'John',
+        'lastname' => 'Doe',
+        'status' => 'active',
+        'created_at' => '01/01/2021 00:00:00',
+        'age' => 30,
+    ]);
+
+    // NOTE: hidden properties
+
+    expect($dto->toArray())
+        ->not()->toHaveKey('password')
+        ->not()->toHaveKey('status')
+        ->not()->toHaveKey('id');
+});
+
+it('casts properties', function () {
     $dto = UserDto::make([
         'firstname' => 'John',
         'lastname' => 'Doe',
@@ -34,38 +51,61 @@ it('toArray returns an array', function () {
         ],
     ]);
 
-    expect($dto->firstname)->toBe('John');
-    expect($dto->lastname)->toBe('Doe');
-    expect($dto->age)->toBe(30);
-    expect($dto->password)->toBe('$password');
+
+    expect($dto)
+        ->firstname->toBe('John')
+        ->lastname->toBe('Doe')
+        ->age->toBe(30)
+        ->password->toBe('$password');
 
     // NOTE: casted properties
 
-    expect($dto->created_at)->toBeInstanceOf(Carbon::class);
-    expect($dto->roles)->toBeInstanceOf(Collection::class);
-    expect($dto->roles?->first())->toBeInstanceOf(RoleDto::class);
-    expect($dto->manager)->toBeInstanceOf(UserDto::class);
-    expect($dto->status)->toBeInstanceOf(UserStatus::class);
-
-    expect($dto->toArray())->toBeArray();
-
-    // NOTE: hidden when to array is called
-
-    expect($dto->toArray())->not()->toHaveKey('password');
-    expect($dto->toArray())->not()->toHaveKey('status');
-    expect($dto->toArray())->not()->toHaveKey('id');
+    expect($dto)
+        ->created_at->toBeInstanceOf(Carbon::class)
+        ->roles->toBeInstanceOf(Collection::class)
+        ->roles?->first()->toBeInstanceOf(RoleDto::class)
+        ->manager->toBeInstanceOf(UserDto::class)
+        ->status->toBeInstanceOf(UserStatus::class);
 });
 
-it('IsPositive throws a validation exception', function () {
+it('throws a validation exception with IsPositive validator', function () {
     UserDto::make([
+        'created_at' => '01/01/2021 00:00:00',
         'firstname' => 'John',
         'lastname' => 'Doe',
         'status' => 'active',
         'age' => -30,
-        'created_at' => '01/01/2021 00:00:00',
     ]);
 })
     ->throws(
         ValidationException::class,
         "The attribute age must be positive '-30' given."
     );
+
+it('returns an array when toArray is called', function () {
+    $dto = UserDto::make([
+        'firstname' => 'John',
+        'lastname' => 'Doe',
+        'status' => 'active',
+        'age' => 30,
+        'manager' => [
+            'firstname' => 'Jimmy',
+            'lastname' => 'Pain',
+            'age' => 25,
+            'status' => 'inactive',
+            'password' => 'jp$password',
+            'created_at' => '02/01/2021 00:00:00',
+            'roles' => [
+                ['name' => 'manager', 'id' => 3],
+            ],
+        ],
+        'password' => '$password',
+        'created_at' => '01/01/2021 00:00:00',
+        'roles' => [
+            ['name' => 'admin', 'id' => 1],
+            ['name' => 'user', 'id' => 2],
+        ],
+    ]);
+
+    expect($dto->toArray())->toBeArray();
+});
